7cc6caaa9659ca2fc94595a1430b77835cca3f53
kenilgamer <kenilk677@gmail.com>
2025-05-29 17:35:06 +0530

    Add server script to package.json and enhance login error handling

--- src/actions/user/authentication/login.ts
+++ src/actions/user/authentication/login.ts
@@ -15,8 +15,9 @@ export default async function Login(values: any) {
     try {
         const user = await User.findOne({ email: validate.data.email })
         if (!user) return { error: 'Incorrect email or password' }
+        if (!user.password) return { error: 'Invalid user account' }
         if (!validate.data.password) return { error: 'Password is required' }
-        const doesPassMatch = await argon.verify(user.password, validate.data.password)
+        const doesPassMatch = await argon.verify(user.password as string, validate.data.password)
         if (!doesPassMatch) return { error: 'Incorrect email or password' }
         const res = await auth.createSession({ userId: user._id.toString(), expiresIn: 1000 * 60 * 60 * 24 * 30 })
         if (res.error) return { error: res.error }

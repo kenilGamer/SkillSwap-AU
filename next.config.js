963d39b5a319a642513e34c7c11dfc7ae06704f6
kenilgamer <kenilk677@gmail.com>
2025-05-29 18:16:56 +0530

    ssasd

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            // Don't resolve 'fs' module on the client to prevent this error
+            config.resolve.fallback = {
+                fs: false,
+                path: false,
+                os: false,
+            }
+        }
+        return config
+    },
+}
+
+module.exports = nextConfig 
